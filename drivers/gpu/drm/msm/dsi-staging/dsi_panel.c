6d1025d8992b0d6ddf78bfbf94dd6ab6a70e8190
Fiqri Ardyansyah <fiqri0927936@gmail.com>
2023-09-06 08:07:12 +0700

    drm: msm: Remove redundant bl_lvl checks in dsi_panel_set_backlight()
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -981,13 +981,10 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 			return 0;
 		}
 
-#ifdef CONFIG_EXPOSURE_ADJUSTMENT
 	if (bl_lvl > 0)
 		bl_lvl = ea_panel_calc_backlight(bl_lvl < bl_dc_min ? bl_dc_min : bl_lvl);
-#endif
-
+	else
 		dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_DISP_DIMMINGOFF);
-	}
 
 	switch (bl->type) {
 	case DSI_BACKLIGHT_WLED:
